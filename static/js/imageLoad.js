32a2429be76add13f6ff5f8db569ee9ea88666fc
Matias <matias.j4rv3np44@gmail.com>
2020-06-22 18:46:07 +0300

    image quality (first loads smaller image)

--- static/js/imageLoad.js
+++ static/js/imageLoad.js
@@ -4,6 +4,9 @@ window.addEventListener('load', function() {
 window.addEventListener('load', function() {
     loadHighResImage(document.getElementById('Led-kuutio-inside2'), '/img/Led-kuutio-inside2.jpg')
 })
+window.addEventListener('load', function() {
+    loadHighResImage(document.getElementById('Led-kuutio'), '/img/Led-kuutio.jpg')
+})
 
 function loadHighResImage(elem, highResUrl) {
     let image = new Image()
