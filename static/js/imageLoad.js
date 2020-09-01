3cfbcf857c199365ec462baab085ee5c60ef7575
Matias <matias.j4rv3np44@gmail.com>
2020-09-01 14:28:39 +0300

    Trying to make the image loader better

--- static/js/imageLoad.js
+++ static/js/imageLoad.js
@@ -1,38 +1,24 @@
-function imageLoader(idd, image){
-  console.log(document.getElementById(idd));
-  if(document.getElementById(idd) !== null){
-      console.log(idd);
+function imageLoader(id, image) {
+  if(document.getElementById(id) !== null){
     window.addEventListener('load', function() {
-        loadHighResImage(document.getElementById(idd), image);
+        loadHighResImage(document.getElementById(id), image)
     })
   }
 }
 
-function loadImages(){
-  imageLoader('profile-pic', '/img/profile-pic.jpg');
-  imageLoader('Led-kuutio-inside1', '/img/Led-kuutio-inside1.jpg');
-  imageLoader('Led-kuutio-inside2', '/img/Led-kuutio-inside2.jpg');
-  imageLoader('Led-kuutio', '/img/Led-kuutio.jpg');
-  imageLoader('quadcopter-new', '/img/quadcopter-new.jpg');
-  imageLoader('quadcopter-old', '/img/quadcopter-old.jpg');
-  imageLoader('globe', '/img/globe.jpg');
-  imageLoader('printer', '/img/printer.jpg');
-  imageLoader('boat','/img/boat.JPG');
-  imageLoader('printer-electronics', '/img/printer-electronics.JPG')
-}
-
-
-
-
-/*
-
-window.addEventListener('load', function() {
-    loadHighResImage(document.getElementById('printer-electronics'), '/img/printer-electronics.JPG')
-})
-*/
+imageLoader('profile-pic', '/img/profile-pic.jpg');
+imageLoader('Led-kuutio-inside1', '/img/Led-kuutio-inside1.jpg');
+imageLoader('Led-kuutio-inside2', '/img/Led-kuutio-inside2.jpg');
+imageLoader('Led-kuutio', '/img/Led-kuutio.jpg');
+imageLoader('quadcopter-new', '/img/quadcopter-new.jpg');
+imageLoader('quadcopter-old', '/img/quadcopter-old.jpg');
+imageLoader('globe', '/img/globe.jpg');
+imageLoader('printer', '/img/printer.jpg');
+imageLoader('boat','/img/boat.JPG');
+imageLoader('printer-electronics', '/img/printer-electronics.JPG')
 
 function loadHighResImage(elem, highResUrl) {
-  console.log(elem)
+    console.log(elem)
     let image = new Image()
     image.addEventListener('load', () => elem.src = highResUrl)
     image.src = highResUrl
